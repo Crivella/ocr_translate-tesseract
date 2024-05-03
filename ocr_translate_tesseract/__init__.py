33d81f2b3decc6962029e47d8435ede7ca8871cc
crivella <davide.grassano@epfl.ch>
2024-05-03 10:43:46 +0200

    Bumb version number

--- ocr_translate_tesseract/__init__.py
+++ ocr_translate_tesseract/__init__.py
@@ -18,7 +18,7 @@
 ###################################################################################
 """Plugin to implement tesseract OCR for ocr_translate."""
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 tesseract_ocr_model_data = {
     'name': 'tesseract',
